baf2dc68b08fee5d6a8483738be8848804a31c20
NOVOMIND\dacharya <Shrey@dhung@n@123>
2022-06-05 00:11:04 +0200

    firstMessage

---
+++ src/main/java/herokuthree/HerokuthreeApplication.java
@@ -0,0 +1,24 @@
+package herokuthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HerokuthreeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HerokuthreeApplication.class, args);
+  }
+}
+
+@RestController
+class HelloController {
+  @GetMapping("/")
+  public String hello() {
+    return "Hello, World!";
+  }
+}
+//
+
